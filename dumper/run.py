861f6e0ffb5c0521930bf66b140c4c19dc9f8bbc
Isaac Chua <yiesheng.chua@gmail.com>
2016-06-24 00:27:11 +1000

    Fix import error when running as script

--- dumper/run.py
+++ dumper/run.py
@@ -1,8 +1,11 @@
 import logging
 import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
 from dumper.dir_monitor import DirMonitor
-
+    
 class Dumper():
 
     @staticmethod
